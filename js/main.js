b92dda1db8987fce5737341f3d5785b85f5232d6
max-malyavin <max_malyavin@mail.ru>
2019-11-27 23:13:53 +0300

    Portfolio 1

---
+++ js/main.js
@@ -0,0 +1,63 @@
+$(function () {
+    $('.slider__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 770,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 550,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 550,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+
+        ]
+    });
+
+
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').slideToggle();
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    var mixer = mixitup('.team__inner-box');
+});
+
